4b1eded64de18c3ef13a0e8d092259c5744f4f2c
israbuilder <israel@xencar.mx>
2026-09-04 10:36:51 -0500

    dashboard update new salesman, og image

--- resources/views/components/layouts/tv.blade.php
+++ resources/views/components/layouts/tv.blade.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html
-    lang="es"
-    class="h-full bg-slate-950"
->
+<html lang="es" class="h-full bg-slate-950">
 <head>
     <meta charset="UTF-8">
 
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-    >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta
-        name="robots"
-        content="noindex, nofollow"
-    >
+    <meta name="robots" content="noindex, nofollow">
 
+    <meta property="og:title" content="Sales Dashboard" />
+    <meta property="og:description" content="Hugo Plumbing Sales Dashboard" />
+    <meta property="og:url" content=https://crm.hugoplumbingtx.com />
+    <meta property="og:image" content="{{assets('assets/img/dashboard.jpg')}}" />
+    <meta property="og:type" content="" />
+    <meta property="og:site_name" content=Hugo Plumbing Dashboard/>
+    <meta name="twitter:card" content="" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content="{{assets('assets/img/dashboard.jpg')}}" />
     <title>
         {{ $title ?? 'Sales TV Dashboard' }}
     </title>
@@ -28,9 +29,7 @@
     @livewireStyles
 </head>
 
-<body
-    class="h-full overflow-hidden bg-slate-950 text-white antialiased"
->
+<body class="h-full overflow-hidden bg-slate-950 text-white antialiased">
     {{ $slot }}
 
     @livewireScripts
