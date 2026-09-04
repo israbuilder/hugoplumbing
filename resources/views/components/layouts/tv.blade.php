d3d16829c26ae2f472d34a435234175bb13abea2
israbuilder <israel@xencar.mx>
2026-09-04 10:42:21 -0500

    fix assets error

--- resources/views/components/layouts/tv.blade.php
+++ resources/views/components/layouts/tv.blade.php
@@ -10,13 +10,13 @@
     <meta property="og:title" content="Sales Dashboard" />
     <meta property="og:description" content="Hugo Plumbing Sales Dashboard" />
     <meta property="og:url" content=https://crm.hugoplumbingtx.com />
-    <meta property="og:image" content="{{assets('assets/img/dashboard.jpg')}}" />
+    <meta property="og:image" content="{{asset('assets/img/dashboard.jpg')}}" />
     <meta property="og:type" content="" />
     <meta property="og:site_name" content=Hugo Plumbing Dashboard/>
     <meta name="twitter:card" content="" />
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
-    <meta name="twitter:image" content="{{assets('assets/img/dashboard.jpg')}}" />
+    <meta name="twitter:image" content="{{asset('assets/img/dashboard.jpg')}}" />
     <title>
         {{ $title ?? 'Sales TV Dashboard' }}
     </title>
