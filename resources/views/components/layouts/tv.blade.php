81560d47c23ec90e27f6c9ee76b363b3c9068983
israbuilder <israel@xencar.mx>
2026-09-04 10:58:43 -0500

    new OG

--- resources/views/components/layouts/tv.blade.php
+++ resources/views/components/layouts/tv.blade.php
@@ -10,13 +10,20 @@
     <meta property="og:title" content="Hugo Plumbing" />
     <meta property="og:description" content="Hugo Plumbing Sales Dashboard" />
     <meta property="og:url" content="https://crm.hugoplumbingtx.com/tv/race/dashboard" />
-    <meta property="og:image" content="{{asset('assets/img/dashboard.jpg')}}" />
-    <meta property="og:type" content="" />
-    <meta property="og:site_name" content="Hugo Plumbing Dashboard"/>
-    <meta name="twitter:card" content="" />
+
+    <meta property="og:image" content="{{ asset('assets/img/dashboard.jpg') }}" />
+    <meta property="og:image:secure_url" content="{{ asset('assets/img/dashboard.jpg') }}" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="576" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Hugo Plumbing Dashboard" />
+
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="Dashboard" />
     <meta name="twitter:description" content="Sales Dashboard" />
-    <meta name="twitter:image" content="{{asset('assets/img/dashboard.jpg')}}" />
+    <meta name="twitter:image" content="{{ asset('assets/img/dashboard.jpg') }}" />
     <title>
         {{ $title ?? 'Sales TV Dashboard' }}
     </title>
